3c5af50f0023a7400d5b5e52092d3a3430f1a107
Lehlogonolo <codesenju@gmail.com>
2019-03-13 16:53:32 +0200

    Update auto_gen.sh

--- auto_gen.sh
+++ auto_gen.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+read -p "Enter slave ip address $ip: " ip
+read -p "Enter slave ip username $usr: " usr
+read -p "Enter slave password $pass: " pass
+rm -f ~/.ssh/id_rsa.pub && rm -f ~/.ssh/id_rsa
+ssh-keygen -b 4096 -f ~/.ssh/id_rsa -N ""
+sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no $usr@$ip
