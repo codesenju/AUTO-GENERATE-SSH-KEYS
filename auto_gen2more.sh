a84fc88f2ba31ce9232bf49aac16fade9270a5b4
Lehlogonolo <codesenju@gmail.com>
2019-03-28 15:49:07 +0200

    Create auto_gen2more.sh

---
+++ auto_gen2more.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Enter password for all your nodes $pass: " pass
+if [ -f server.txt ]; then
+for server in $(cat server.txt); do
+rm -f ~/.ssh/id_rsa.pub && rm -f ~/.ssh/id_rsa
+ssh-keygen -b 4096 -f ~/.ssh/id_rsa -N ""
+sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no root@$server
+echo 'copied to ' $server
+done
+else
+echo 'No server.txt file'
+fi
