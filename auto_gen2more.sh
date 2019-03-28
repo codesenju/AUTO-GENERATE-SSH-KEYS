6f38f50e7e2326906987300667acfaa4e34c5dc1
Lehlogonolo <codesenju@gmail.com>
2019-03-28 15:50:29 +0200

    Update auto_gen2more.sh

--- auto_gen2more.sh
+++ auto_gen2more.sh
@@ -5,7 +5,7 @@ for server in $(cat server.txt); do
 rm -f ~/.ssh/id_rsa.pub && rm -f ~/.ssh/id_rsa
 ssh-keygen -b 4096 -f ~/.ssh/id_rsa -N ""
 sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no root@$server
-echo 'copied to ' $server
+echo 'SSH key copied to ' $server ' successfully!'
 done
 else
 echo 'No server.txt file'
